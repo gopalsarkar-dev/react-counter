e29ed18e8aec8c6513eb7ed642cfa2c6f7bf3f33
Gopal Sarkar <gopalsarkar.dev@gmail.com>
2025-05-19 03:51:08 +0530

    Counter Design done

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,22 @@
 const App = () => {
   return (
     <>
-      <div className="text-5xl text-center">Ract-Vit</div>
+      <div className="grid place-items-center h-screen">
+        <div className="bg-white dark:bg-gray-50 rounded-lg px-6 py-8 ring shadow-xl ring-gray-900/5">
+          <div className="grid place-items-center gap-3">
+            <div className="text-2xl">React Counter</div>
+            <div className="text-3xl">0</div>
+            <div className="flex gap-2">
+              <button className="p-1 px-2 bg-neutral-300 rounded-sm w-24">
+                Increnent
+              </button>
+              <button className="p-1 px-2 bg-neutral-300 rounded-sm w-24">
+                Decrement
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
     </>
   );
 };
